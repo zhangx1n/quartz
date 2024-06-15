43f5e82590ba8e951560e58880661628750f214d
zhangx1n <48432214+zhangx1n@users.noreply.github.com>
2024-06-15 11:48:05 +0800

    Quartz sync: Jun 15, 2024, 11:48 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.RecentNotes({limit: 5}),
+    Component.DesktopOnly(Component.RecentNotes({ limit: 5 })),
   ],
   right: [
     Component.Graph(),
@@ -45,7 +45,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
-    Component.DesktopOnly(Component.RecentNotes({limit: 5}),
+    Component.DesktopOnly(Component.RecentNotes({ limit: 5 })),
   ],
   right: [],
 }
